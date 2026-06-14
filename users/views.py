9c0025a94df9d5d597a2d8d1daca8bbb28255eda
brakobby <nkobby454@gmail.com>
2026-06-14 02:41:28 +0000

    fixed the postgre issues & made migrations

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from rest_framework.response import Respose
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
