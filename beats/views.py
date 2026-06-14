b70411a7e73c075549d96c533aac748b8c3afe1f
brakobby <nkobby454@gmail.com>
2026-06-14 03:25:08 +0000

    added part of views

--- beats/views.py
+++ beats/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from .models import Genre, Beat
+from .serializers import BeatSerializer, GenreSerializer
 
-# Create your views here.
+
+class BeatListView(generics.ListAPIView):
+    serializer_class = BeatSerializer
+    permission_class = [permissions.AllowAny]
+    filter_backends = [filters.searchFilter]
+    search_fields = ['title', 'genre__name','mood']
